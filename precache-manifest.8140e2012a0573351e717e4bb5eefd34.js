595fcb9474ab6ba435c78111d5cf4fb4f7c96a50
SJVoon <brucev90@gmail.com>
2021-01-16 22:36:19 +0800

    adding dist subtree to deploy

---
+++ precache-manifest.8140e2012a0573351e717e4bb5eefd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc359e98d5bd042e132",
+    "url": "/vue-my-account/css/app.1ea69065.css"
+  },
+  {
+    "revision": "fc522da9e553f2b7f3a4",
+    "url": "/vue-my-account/css/chunk-vendors.fdcf31ba.css"
+  },
+  {
+    "revision": "61ca103ff2314f93ca7db6538be14398",
+    "url": "/vue-my-account/index.html"
+  },
+  {
+    "revision": "ddc359e98d5bd042e132",
+    "url": "/vue-my-account/js/app.defe00ba.js"
+  },
+  {
+    "revision": "fc522da9e553f2b7f3a4",
+    "url": "/vue-my-account/js/chunk-vendors.c02b84e2.js"
+  },
+  {
+    "revision": "e6c01c7e2b0b218a44f450829cc014a0",
+    "url": "/vue-my-account/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-my-account/robots.txt"
+  }
+]);
