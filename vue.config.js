4015a5c929746c183057f53ab98c1b6e74c3c71c
SJVoon <brucev90@gmail.com>
2021-01-16 22:30:37 +0800

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/hello-world/'
-    : '/',
+  publicPath: '/vue-my-account/',
   "transpileDependencies": [
     "vuetify"
   ]
